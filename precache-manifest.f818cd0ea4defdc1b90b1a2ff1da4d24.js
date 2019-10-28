1b0371f05f69264fc91a5feaebe81bd524b0f049
hkh7670 <hkh7670@gmail.com>
2019-10-28 16:34:12 +0900

    Updates

---
+++ precache-manifest.f818cd0ea4defdc1b90b1a2ff1da4d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee124097c74affed88fb549b7ba3772",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "27c8021460cf1cefc887",
+    "url": "/movie_app_2019/static/css/main.54f1f796.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "27c8021460cf1cefc887",
+    "url": "/movie_app_2019/static/js/main.3d7f3062.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
